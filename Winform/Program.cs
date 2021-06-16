da34d7846ad1665a1fb6ad15402ae38d553806bb
Martin Suffer <54630117+RossetaGotStoned@users.noreply.github.com>
2021-06-16 11:42:08 +0430

    Winform Source

---
+++ Winform/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Winform
+{
+    static class Program
+    {
+        static Window window;
+
+        [DllExport]
+        static IntPtr CreateForm()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            window = new Window();
+            window.Size = new System.Drawing.Size(1,1);
+            window.StartPosition = FormStartPosition.Manual;
+            window.Location = new System.Drawing.Point(-60, -60);
+            window.Show();
+            return window.Handle;
+        }
+    }
+}
